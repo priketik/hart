abe013ce283dab8d22f39384232ed87e10eba20b
priketik <124594802+priketik@users.noreply.github.com>
2023-02-06 16:04:10 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.pause(2000)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(2000)
+})
